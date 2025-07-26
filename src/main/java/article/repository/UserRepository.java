cb9f996325d1eeedc93fe437c79c108d25f76821
StoneBlood <brkalix@gmail.com>
2025-07-26 13:49:34 +0300

    created UserRepository

---
+++ src/main/java/article/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package article.repository;
+
+import article.model.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+
+    @EntityGraph(attributePaths = "role")
+    Optional<User> findByEmail(String email);
+}
