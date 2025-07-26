d9e381fe2959a688056327bd0c7cd678c683635e
StoneBlood <brkalix@gmail.com>
2025-07-26 20:09:52 +0300

    created ArticleService

---
+++ src/main/java/article/service/article/ArticleService.java
@@ -0,0 +1,12 @@
+package article.service.article;
+
+import article.dto.article.ArticleRequestDto;
+import article.dto.article.ArticleResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ArticleService {
+    ArticleResponseDto create(ArticleRequestDto requestDto);
+
+    Page<ArticleResponseDto> findAll(Pageable pageable);
+}
